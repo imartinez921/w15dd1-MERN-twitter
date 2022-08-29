aba104a8cc80d013185b230d457aa2683ea3f9fa
Irene Martinez <imartinez921@gmail.com>
2022-08-29 01:33:44 -0400

    Users, user registration and login, validations and passport, Tweets routes; install front end

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-const express = require("express");
-const app = express(); // will give back the app obj that we can then configure
-
-const mongoose = require ('mongoose');
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+const User = require("./models/User");
 
-  app.get("/", (req, res) => {
-      res.send("Booyah baby!")
-    }); //app will listen for any get requests on the first argument, the root route
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-app.use("/api/users", users);
+const express = require("express");
+const app = express();
+
+
+  app.get("/", (req, res) => {
+    const user = new User ({
+      handle: "jim",
+      email: "jim@jim.jim",
+      password: "jimisgreat"
+    })
+    user.save();
+    res.send("Booyah baby!")
+  }); //app will listen for any get requests on the first argument, the root route
+app.use("/api/users", users); //Any path that matches this route will send it to the route file
 app.use("/api/tweets", tweets);
 
-const bodyParser = require ('body-parser');
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 
 const port = process.env.PORT || 5000; // if we are in production, use that port variable, else use 5000
 
